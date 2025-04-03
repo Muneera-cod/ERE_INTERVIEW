6ee7671143679a1d4e26bbc4dd334487b5e69958
Muneera <muneerakottarath99@gmail.com>
2025-04-03 14:54:07 +0530

    fourth commit

--- src/components/Pages/MainPage.jsx
+++ src/components/Pages/MainPage.jsx
@@ -1,28 +1,8 @@
 import { useEffect, useState } from 'react'
 import Table from '../Ui/Table'
-import SearchBar from '../Ui/SearchBar'
-// const url = 'https://dummyjson.com/users'
-function MainPage() {
-    // const [data, setData] = useState([])
-    // const [loading, setLoading] = useState(false)
-    // const [error, setError] = useState(null)
 
-    // const fetchData = async () => {
-    //     setLoading(true)
-    //     try {
-    //         const response = await fetch(url)
-        
-    //         const result = await response.json()
-    //         setData(result.users)
-    //     } catch (error) {
-    //         setError(error.message)
-    //     } finally {
-    //         setLoading(false)
-    //     }
-    // }
-    // useEffect(()=>{
-    //     fetchData()
-    // },[])
+function MainPage() {
+  
   return (
     <div className='outer-container'>
         <Table/>
